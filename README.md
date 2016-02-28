56ecb16e385eb412ee8c57b4bcae9e3192053a02
Bahul Jain <bkj2111@columbia.edu>
2016-02-27 23:39:29 -0500

    added req, pretty_printer and cleaned readme

--- README.md
+++ README.md
@@ -1,15 +1,39 @@
 # Twitter Keyword search
 
-This program returns a random tweet based on a keyword search. The program accepts keywords from command prompt.
+This program returns a random tweet based on a keyword search. The program
+accepts keywords from command prompt.
 
 ### Setup
 
-1. All the dependencies for the program to run are listed in the requirements.txt file.
+1. Clone this repository or download it as a zip and then unzip it.
 
-2. Now to install all the requirements run the following command.
+2. All the dependencies for the program to run are listed in the
+requirements.txt file.
 
-```
-$ pip install -r requirements.txt
-```
+3. Now to install all the requirements run the following command. An alternate
+and cleaner way would be to create a virtual machine and run the command below
+inside the virtual environment.
 
-3. Done
+  ```
+  $ pip install -r requirements.txt
+  ```
+
+4. Done
+
+### Run the program
+
+- To run the program, simply run the following command in the terminal from within
+the repository.
+
+  ```
+  $ python tweet-search.py 'keyword-1' 'keyword-2' 'keyword-3'
+  ```
+
+- Any number of space separated single/double-quoted keywords can be passed as
+arguments.
+
+- A tweet will be searched and printed for each keyword entered.
+
+
+
+Any feedback is appreciated :)
--- tweet-search.py
+++ tweet-search.py
@@ -1,8 +1,9 @@
 import tweepy
 import time
-import json
 import sys
+import random as r
 
+# Decorator function for rate limiting function calls.
 def RateLimited(maxPerSecond):
     minInterval = 1.0 / float(maxPerSecond)
     def decorate(func):
@@ -21,7 +22,7 @@ def RateLimited(maxPerSecond):
 class Twitter:
 
     def __init__(self, consumer_key=None, consumer_secret=None):
-        #Variables that contains the application-only credentials to access Twitter API
+        # Variables that contains the application-only credentials to access Twitter API
         if consumer_key is None:
             consumer_key = "Tq20eDbLhvBBGgK2jXcp8Faif"
         if consumer_secret is None:
@@ -30,24 +31,39 @@ class Twitter:
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         self.api = tweepy.API(auth)
 
-    # In application-only auth, the application can make 450 queries/requests
-    # per 15 minutes or equivalently 1 query per 2 seconds.
+    '''
+     In application-only auth, the application can make 450 queries/requests
+     per 15 minutes or equivalently 1 query per 2 seconds.
+    '''
     @RateLimited(0.5) # 1 query per 2 seconds
     def keyword_search(self, keyword=None):
         if keyword:
+            print "Keyword: ", keyword
             try:
                 result = self.api.search(q = keyword, rpp = 1)
-                tweet = result[0]
+                tweet = result[r.randint(0, len(result) - 1)]
 
-                text = tweet.text.encode('ascii', 'ignore')
-                user_handle = "@" + tweet.author.screen_name
-
-                print user_handle + ": " + text
+                return tweet
             except tweepy.TweepError, err:
                 err = err.message[0]
                 print "Tweepy Error: " + `err['code']` + " - " +  err['message']
         else:
-            print "Please enter a valid keyword"
+            print "'keyword' field required. Cannot be empty or null."
+
+    def pretty_print_tweet(self, tweet=None):
+        if tweet:
+            try:
+                text = tweet.text.encode('ascii', 'ignore')
+                user_handle = "@" + tweet.author.screen_name
+
+                print "User: ", user_handle
+                print "Tweet: ", text
+            except Exception as e:
+                print e
+            print ""
+        else:
+            print "'tweet' field required. Cannot be empty or null."
+
 
 if __name__=="__main__":
 
@@ -57,5 +73,5 @@ if __name__=="__main__":
     twitter = Twitter()
 
     for keyword in keywords:
-        twitter.keyword_search(keyword)
-        print ""
+        tweet = twitter.keyword_search(keyword)
+        twitter.pretty_print_tweet(tweet)
